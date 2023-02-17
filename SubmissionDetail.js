b22b9ca1508d5a955a7551db7dafbc8c1746db75
BasharElmetwaliahmed <113910248+BasharElmetwaliahmed@users.noreply.github.com>
2023-02-17 22:01:39 +0200

    Create SubmissionDetail.js

---
+++ SubmissionDetail.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isPerfectSquare = function(num) {
+    if(num==1){
+        return true
+    }
+    for(let i=0;i<num;i++){
+  if((i*i)==num){
+    return true
+  }
+  else if((i*i)>num){
+    return false
+  }
+}
+return false
+    
+};
